7c7f6526435236a6bc0f00a49838575351dc3d1a
sidhantverma <sidhant@saah-capital.com>
2025-12-02 00:25:58 +0530

    Working on XML layout and structuring...

--- app/src/main/java/com/example/notesmanager/ui/AddNoteBottomSheet.kt
+++ app/src/main/java/com/example/notesmanager/ui/AddNoteBottomSheet.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.children
 import com.example.notesmanager.R
 import com.example.notesmanager.databinding.SheetAddNoteBinding
@@ -19,25 +20,47 @@ class AddNoteBottomSheet(
     override fun getTheme(): Int = R.style.BottomSheetDialogTheme
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = SheetAddNoteBinding.inflate(inflater, container, false)
 
         binding.btnAdd.setOnClickListener {
 
-            val title = binding.etTitle.text.toString()
-            val body = binding.etBody.text.toString()
+            val title = binding.etTitle.text.toString().trim()
+            val body = binding.etBody.text.toString().trim()
 
-            val selectedTags = binding.chipGroupTags.children
-                .filter { (it as Chip).isChecked }
-                .map { (it as Chip).text.toString() }
-                .toList()
+            val selectedTags =
+                binding.chipGroupTags.children.filter { it is Chip && (it as Chip).isChecked }
+                    .map { (it as Chip).text.toString() }.toList()
+
+            if (title.isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.please_enter_a_title), Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            if (body.isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.please_enter_body), Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            if (selectedTags.isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.please_select_at_least_one_tag), Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
 
             onAdd(title, body, selectedTags)
             dismiss()
         }
+
 
         return binding.root
     }
